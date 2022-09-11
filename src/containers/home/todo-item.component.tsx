1df1e2457a065633ccafbacf091d67bfbd13a398
Radi Rusadi <radi.rusadi.s@gmail.com>
2022-09-11 18:44:08 +0700

    Fixed issue item scroll on android

--- src/containers/home/todo-item.component.tsx
+++ src/containers/home/todo-item.component.tsx
@@ -87,7 +87,7 @@ const TodoItem = ({ todo, onPressComplete, onPressDelete }: TodoProps) => {
       style={[s.container, rTodoContainerStyle]}
       entering={SlideInRight.delay(100)}
       layout={Layout.springify()}>
-      <PanGestureHandler onGestureEvent={panGesture}>
+      <PanGestureHandler onGestureEvent={panGesture} activeOffsetX={-1}>
         <Animated.View style={[s.shadow, rStyle]}>
           <TouchableOpacity
             key={todo.id}
